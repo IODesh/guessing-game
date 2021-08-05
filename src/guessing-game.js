fdf49b90b69166c6c71e2d1b2ce3e9030ac4fed1
Igor Desh <desh_gambler@mail.ru>
2021-08-05 22:01:11 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.number = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.number = Math.round((min + max) / 2);
     }
 
     lower() {
-
+        this.max = this.number;
     }
 
     greater() {
-
+        this.max = this.number;
     }
 }
 
 module.exports = GuessingGame;
+
